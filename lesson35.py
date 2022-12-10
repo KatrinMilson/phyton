fa1075ee995f6c5e5fcfba4a8cfcf96cdff43c12
SatoAkixiro <102458052+KatrinMilson@users.noreply.github.com>
2022-12-10 19:25:57 +0300

    Add files via upload

---
+++ lesson35.py
@@ -0,0 +1,33 @@
+#исключения
+# print('hello')
+# print(int(100 / 10))
+# print(1 + '2')
+# print(int('test'))
+# try:
+#     num = 100 / 0
+# except ZeroDivisionError:
+#     num = 0
+# except TypeError:
+#     num = 1
+# print(num)
+
+# try:
+#     num = 100 / 2
+# except Exception:
+#     num = 0
+# else:
+#     print('Else')
+# finally:
+#     print('Finally')
+# print(num)
+# print('Hi')
+#
+# while True:
+#     try:
+#         num = int(input('Enter your number: '))
+#         print(f'100 / {num} = {100 / num}')
+#     except ZeroDivisionError:
+#         print('The number must be greater than zero')
+#     except ValueError:
+#         print('Must be a number')
+
