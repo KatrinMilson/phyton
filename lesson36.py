a7c0e48935f57df3b88d05110d6e4b25bd8b7edd
SatoAkixiro <102458052+KatrinMilson@users.noreply.github.com>
2022-12-15 19:41:49 +0300

    Add files via upload

---
+++ lesson36.py
@@ -0,0 +1,17 @@
+# class A:
+#     property1 = 'Property1'
+#     property2 = 'Property2'
+#
+#     def say_hi(self, name =''):
+#         if name:
+#             return 'Hi, ' + name
+#         else:
+#             return 'Hello, ' + self.name
+#
+#
+# a = A()
+# b = A()
+# print(a.say_hi('John'))
+# print(a.property1)
+
+
